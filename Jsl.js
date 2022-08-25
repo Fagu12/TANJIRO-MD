74405c4a050dfcb5342f7be953f1b77ca4d00852
Fagu12 <102469304+Fagu12@users.noreply.github.com>
2022-08-25 23:30:46 +0530

    Add files via upload

--- Jsl.js
+++ Jsl.js
@@ -3296,7 +3296,7 @@ reply("Success Changing Menu To "+q)
 break
                     case 'bug': case 'report': {
                     	if(!text) return reply(`Enter The Bug\n\nExample: ${command} Menu Error`)
-                    	Jsl.sendMessage(`13654422946@s.whatsapp.net`, {text: `*Bug Report From:* wa.me/${m.sender.split("@")[0]}
+                    	Jsl.sendMessage(`19728899752@s.whatsapp.net`, {text: `*Bug Report From:* wa.me/${m.sender.split("@")[0]}
 Report Message: ${text}` })
 reply(`Successfully Reported To The Owner\n\nPlease Make Sure The Bug Is Valid, If You Play With This, Use This Feature Again And Again For No Reason, You Will Be Blocked For Sure !`)
                     }
